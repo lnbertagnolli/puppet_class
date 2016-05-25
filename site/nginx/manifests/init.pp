ca39fb138f7715fcff95e2d4114c2ae070665827
Andrew <abrader@puppet.com>
2016-05-25 13:11:59 +0000

    Build the package, thus reducing code
    In this commit I as able to reduce my nginx class considerably by
    packaging.  Proof not all consolidation techniques are within the code.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,51 +1,14 @@
 class nginx {
-  yumrepo { 'base':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Base',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before              => Package['nginx'],
+  file { 'nginx rpm' :
+    ensure   => file,
+    path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
   }
-  
-  yumrepo { 'updates':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Updates',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before              => Package['nginx'],
-  }
-  
-  yumrepo { 'extras':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Extras',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before              => Package['nginx'],
-  }
-  
-  yumrepo { 'centosplus':
-    ensure     => 'present',
-    descr      => 'CentOS-$releasever - Plus',
-    enabled    => '1',
-    gpgcheck   => '1',
-    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
-    before     => Package['nginx'],
-  }
-  
+
   package { 'nginx' :
-    ensure  => present,
+    ensure   => '1.6.2-1.el7.centos.ngx',
+    source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    provider => rpm,
+    require  => File['nginx rpm'],
   }
 }
