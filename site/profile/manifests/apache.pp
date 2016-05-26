ef15b93257fe6cd983cb5bd9f3b91c452301c6cd
Andrew Brader <abrader@gmail.com>
2016-05-26 15:56:19 -0400

    Update apache.pp
    Added host record and vhost in order to easily redirect the docket

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,5 +1,15 @@
 class profile::apache {
-  include ::apache
+  host { ::fqdn :
+    ensure => present,
+    ip     => ::ipaddress,
+  }
   
+  include ::apache
   include ::apache::mod::php
+  
+  apache::vhost { ::fqdn :
+    port    => '80',
+    docroot => '/var/www/wordpress',
+  }
+  
 }
