61ec4b8d80f56184c844f4ed577ea10ef244cb75
Andrew Brader <abrader@gmail.com>
2016-05-26 15:57:12 -0400

    Update apache.pp
    Fixed syntax errors and added host alias

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,13 +1,14 @@
 class profile::apache {
-  host { ::fqdn :
-    ensure => present,
-    ip     => ::ipaddress,
+  host { $::fqdn :
+    ensure       => present,
+    host_aliases => $::hostname
+    ip           => $::ipaddress,
   }
   
   include ::apache
   include ::apache::mod::php
   
-  apache::vhost { ::fqdn :
+  apache::vhost { $::fqdn :
     port    => '80',
     docroot => '/var/www/wordpress',
   }
