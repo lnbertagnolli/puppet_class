36a156864eb89c093765fb73ad2d42b5a3ab97e3
Andrew Brader <abrader@gmail.com>
2016-05-26 11:26:21 -0400

    Update init.pp
    Updated base class to reflect the movement of params to their own class.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,12 @@
 class nginx (
-  $root = undef,
-) {
-  case $::osfamily {
-    'redhat','debian': {
-      $package         = 'nginx'
-      $owner           = 'root'
-      $group           = 'root'
-      $default_docroot = '/var/www'
-      $confdir         = '/etc/nginx'
-      $logdir          = '/var/log/nginx'
-    }
-    'windows': {
-      $package         = 'nginx-service'
-      $owner           = 'Administrator'
-      $group           = 'Administrators'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-      $confdir         = 'C:/ProgramData/nginx'
-      $logdir          = 'C:/ProgramData/nginx/logs'
-    }
-    default : {
-      fail("Module ${module_name} is not intended to run on ${::osfamily}")
-    }
-  }
-
-  $docroot = $root ? {
-    undef   => $default_docroot,
-    default => $root,
-  }
-
-  $runas_user = $::osfamily ? {
-    'redhat'  => 'nginx',
-    'debian'  => 'www-data',
-    'windows' => 'nobody',
-  }
-
+  $package    = $nginx::params::package,
+  $owner      = $nginx::params::owner,
+  $group      = $nginx::params::group,
+  $docroot    = $nginx::params::docroot,
+  $confdir    = $nginx::params::confdir,
+  $logdir     = $nginx::params::logdir,
+  $runas_user = $nginx::params::runas_user,
+) inherits nginx::params {
   if $::virtual {
     $virt_name = capitalize($::virtual)
   }
