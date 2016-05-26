c63751dbc52701a8792d9e3f15d46b8f8f050257
Andrew Brader <abrader@gmail.com>
2016-05-26 16:44:28 -0400

    Update wordpress.pp
    Changed back

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,8 +1,3 @@
 class profile::wordpress {
-  class { ::wordpress :
-    db_user     => 'wordpress',
-    db_name     => 'wordpress',
-    db_host     => $::ipaddress_lo,
-    install_dir => '/opt/wordpress',
-  }
+  include ::wordpress
 }
